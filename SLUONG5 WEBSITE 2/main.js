ff61b67af38d69a150a44b53a4ac1ab841b230bd
stephanieluongs <sluong5@asu.edu>
2025-10-03 11:26:24 -0700

    Delete SLUONG5 WEBSITE 2 directory

--- SLUONG5 WEBSITE 2/main.js
+++
@@ -1,8 +0,0 @@
-function emailUs(address, subject = "", body = "") {
-  try { navigator.clipboard?.writeText(address); } catch (_) {}
-  const q = new URLSearchParams();
-  if (address) q.set("to", address);
-  if (subject) q.set("su", subject);
-  if (body) q.set("body", body);
-  window.open("https://mail.google.com/mail/?view=cm&" + q.toString(), "_blank", "noopener");
-}
